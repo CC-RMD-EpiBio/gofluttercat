e11e56d84a8d2f4e1c60bec888d103661596b7f7
joshchangNIH <141774921+joshchangNIH@users.noreply.github.com>
2024-11-13 12:57:10 -0500

    remove item

--- backend-golang/models/score.go
+++ backend-golang/models/score.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	"math"
+	"slices"
 
 	math2 "github.com/CC-RMD-EpiBio/gofluttercat/backend-golang/pkg/math"
 
@@ -59,7 +60,7 @@ func (bs BayesianScorer) Score(resp *SessionResponses) error {
 	return nil
 }
 
-func (bs BayesianScorer) AddResponses(resp []Response) error {
+func (bs *BayesianScorer) AddResponses(resp []Response) error {
 	if len(resp) == 0 {
 		return nil
 	}
@@ -69,16 +70,47 @@ func (bs BayesianScorer) AddResponses(resp []Response) error {
 	}
 
 	ll := bs.Model.LogLikelihood(abilities, resp)
+	for _, r := range resp {
+		bs.Answered = append(bs.Answered, &r)
+	}
 	bs.Running.Energy = vek.Add(bs.Running.Energy, ll.Data)
 	fmt.Printf("ll: %v\n", ll)
 
 	return nil
 }
 
-func (bs BayesianScorer) RemoveResponses(name []string) error {
+func RemoveIndex(s []int, index int) []int {
+	ret := make([]int, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
+func (bs *BayesianScorer) RemoveResponses(itmNames []string) error {
+	toDelete := make([]Response, 0)
+	toDeleteNames := make([]string, 0)
 	for _, r := range bs.Answered {
-		fmt.Printf("r: %v\n", r)
+		if slices.Contains(itmNames, r.Item.Name) {
+			toDelete = append(toDelete, *r)
+			toDeleteNames = append(toDeleteNames, r.Item.Name)
+		}
 	}
+
+	n := 0
+	for _, r := range bs.Answered {
+		if !slices.Contains(toDeleteNames, r.Item.Name) {
+			bs.Answered[n] = r
+			n++
+		}
+	}
+	bs.Answered = bs.Answered[:n]
+
+	abilities, err := ndvek.NewNdArray([]int{len(bs.AbilityGridPts)}, bs.AbilityGridPts)
+	if err != nil {
+		panic(err)
+	}
+	ll := bs.Model.LogLikelihood(abilities, toDelete)
+	bs.Running.Energy = vek.Sub(bs.Running.Energy, ll.Data)
+
 	return nil
 }
 
